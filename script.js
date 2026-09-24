9898aa960c3621f261a54f550b9e76f294fc2f79
bingsew <82799186+bingsew@users.noreply.github.com>
2026-09-24 15:51:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const seal = document.querySelector(".seal");
-
 const envelope = document.querySelector(".envelope");
+const seal = document.querySelector(".seal");
+const letter = document.querySelector(".letter");
+const garden = document.querySelector(".garden");
 
 seal.addEventListener("click", function () {
-
-	envelope.classList.add("open");
+	envelope.classList.toggle("open");
 
 });
+
+letter.addEventListener("click", function () {
+	garden.classList.add("show");
+	envelope.classList.add("hide");
+	
+});
+
+
